54f9af253156b13d906660b4b7982617d78505f3
Denis Gogul <denis19091989@gmail.com>
2026-02-27 15:38:33 +0300

    Добавил пути в файлах 'urls'

--- my_project_mu/src/bookings/urls.py
+++ my_project_mu/src/bookings/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from bookings.views import BookingCreateView, BookingDeleteView, BookingListView
 
 urlpatterns = [
-
+    path("create/", BookingCreateView.as_view(), name="booking-create"),
+    path("list/", BookingListView.as_view(), name="booking-list"),
+    path("<int:booking_id>/delete/", BookingDeleteView.as_view(), name="booking-delete"),
 ]
--- my_project_mu/src/hotels/urls.py
+++ my_project_mu/src/hotels/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from hotels.views import RoomCreateView, RoomDeleteView, RoomListView
 
 urlpatterns = [
-
+    path("create/", RoomCreateView.as_view(), name="room-create"),
+    path("list/", RoomListView.as_view(), name="room-list"),
+    path("<int:room_id>/delete/", RoomDeleteView.as_view(), name="room-delete"),
 ]
